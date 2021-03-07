2c338e76a38ffdb1547f15d6b259e018733772f3
PolandPanda <mateusz.mamica18@gmail.com>
2021-03-07 12:03:51 +0100

    Basic node js tutorial

---
+++ index.js
@@ -0,0 +1,8 @@
+// import 
+const express = require('express');
+//all library is in function&variables
+const app = express();
+
+app.listen(3000, () => console.log("listening..."));
+// serve static content
+app.use(express.static('public'));
